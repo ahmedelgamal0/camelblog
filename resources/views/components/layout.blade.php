61aa89037266f16e9f4bfa025733347846ecb747
ahmedelgamal <a.elgamal@nu.edu.eg>
2022-03-31 18:19:58 +0200

    add the design

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,122 @@
+<!doctype html>
+
+<title>Laravel From Scratch Blog</title>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+
+<body style="font-family: Open Sans, sans-serif">
+    <section class="px-6 py-8">
+        <nav class="md:flex md:justify-between md:items-center">
+            <div>
+                <a href="/">
+                    <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+                </a>
+            </div>
+
+            <div class="mt-8 md:mt-0">
+                <a href="/" class="text-xs font-bold uppercase">Home Page</a>
+
+                <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+                    Subscribe for Updates
+                </a>
+            </div>
+        </nav>
+
+        <header class="max-w-xl mx-auto mt-20 text-center">
+            <h1 class="text-4xl">
+                Latest <span class="text-blue-500">Laravel From Scratch</span> News
+            </h1>
+
+            <h2 class="inline-flex mt-2">By Lary Laracore <img src="/images/lary-head.svg"
+                                                               alt="Head of Lary the mascot"></h2>
+
+            <p class="text-sm mt-14">
+                Another year. Another update. We're refreshing the popular Laravel series with new content.
+                I'm going to keep you guys up to speed with what's going on!
+            </p>
+
+            <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
+                <!--  Category -->
+                <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
+                    <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
+                        <option value="category" disabled selected>Category
+                        </option>
+                        <option value="personal">Personal</option>
+                        <option value="business">Business</option>
+                    </select>
+
+                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
+                         height="22" viewBox="0 0 22 22">
+                        <g fill="none" fill-rule="evenodd">
+                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                            </path>
+                            <path fill="#222"
+                                  d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                        </g>
+                    </svg>
+                </div>
+
+                <!-- Other Filters -->
+                <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
+                    <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
+                        <option value="category" disabled selected>Other Filters
+                        </option>
+                        <option value="foo">Foo
+                        </option>
+                        <option value="bar">Bar
+                        </option>
+                    </select>
+
+                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
+                         height="22" viewBox="0 0 22 22">
+                        <g fill="none" fill-rule="evenodd">
+                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                            </path>
+                            <path fill="#222"
+                                  d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                        </g>
+                    </svg>
+                </div>
+
+                <!-- Search -->
+                <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
+                    <form method="GET" action="#">
+                        <input type="text" name="search" placeholder="Find something"
+                               class="bg-transparent placeholder-black font-semibold text-sm">
+                    </form>
+                </div>
+            </div>
+        </header>
+
+
+
+        <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+            <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
+            <h5 class="text-3xl">Stay in touch with the latest posts</h5>
+            <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
+
+            <div class="mt-10">
+                <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
+
+                    <form method="POST" action="#" class="lg:flex text-sm">
+                        <div class="lg:py-3 lg:px-5 flex items-center">
+                            <label for="email" class="hidden lg:inline-block">
+                                <img src="/images/mailbox-icon.svg" alt="mailbox letter">
+                            </label>
+
+                            <input id="email" type="text" placeholder="Your email address"
+                                   class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+                        </div>
+
+                        <button type="submit"
+                                class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8"
+                        >
+                            Subscribe
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </footer>
+    </section>
+</body>
