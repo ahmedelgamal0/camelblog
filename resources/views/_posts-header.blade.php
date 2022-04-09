83a36d5516e2dbea3fca41decf348b66c9df8198
ahmedelgamal <a.elgamal@nu.edu.eg>
2022-04-09 02:44:44 +0200

    components and route

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -1,9 +1,9 @@
 <header class="max-w-xl mx-auto mt-20 text-center">
             <h1 class="text-4xl">
-                Latest <span class="text-blue-500">Laravel From Scratch</span> News
+                Latest <span class="text-blue-500">Camel</span> News
             </h1>
 
-            <h2 class="inline-flex mt-2">By Lary Laracore <img src="/images/lary-head.svg"
+            <h2 class="inline-flex mt-2">By Ahmed Elgamal <img src="/images/lary-head.svg"
                                                                alt="Head of Lary the mascot"></h2>
 
             <p class="text-sm mt-14">
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,23 +2,15 @@
 	@include ('_posts-header')
 
 	<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
 
-		<x-post-featured-card />
+            <x-posts-grid :posts="$posts"/>
 
-		<div class="lg:grid lg:grid-cols-2">
 
-			<x-post-card />
-            <x-post-card />
+        @else
+        	<p class="text-center">No posts yet. Please check back later.</p>
+        @endif
 
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-             
-        </div>
     </main>
 </x-layout>
 
